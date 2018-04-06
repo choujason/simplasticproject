c9bb5538dc31958243e316a9085c19070af099aa
choujason <jasonya2002@gmail.com>
2018-04-06 18:27:37 +0800

    fix process bar
    fix process bar

--- upload.js
+++ upload.js
@@ -20,7 +20,7 @@ initApp = function () {
 
 
             user.getIdToken().then(function (accessToken) {
-                document.getElementById('sign-in-status').textContent = '分享些你重複使用塑膠袋的方式吧！';
+                document.getElementById('sign-in-status').textContent = '親愛的'+user.displayName+'，今天過得好嗎？每週只需要上傳1次Simplastic使用狀況，就可以參加每週的商品禮卷抽獎喔！當然，傳得越多，中獎機率越高喔';
                 document.getElementById('sign-in').textContent = 'Hello, ' + user.displayName;
                 //                        document.getElementById('account-details').textContent = JSON.stringify({
                 //                            displayName: displayName,
@@ -35,49 +35,39 @@ initApp = function () {
             });
 
             //upload image
-            var uploader = document.getElementById('uploader');
-            var fileButton = document.getElementById('fileButton');
-            var state = document.getElementById('state');
-            // listen for file selection
-            fileButton.addEventListener('change', function (e) {
-                //get file
-                var file = e.target.files[0];
-                //create ref
-                var storageRef = firebase.storage().ref(user.displayName.toString());
-                var imgRef = storageRef.child(file.name + '-' + Date());
-                //upload file
-                var task = imgRef.put(file);
-                //update progress bar
-                task.on('state_changed',
-                    function progress(snapshot) {
-                        var percentage = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
-                        uploader.value = percentage;
-                        $("#uploader")
-                            .css("width", percentage + "%")
-                            .attr("aria-valuenow", percentage)
-                            .text(percentage + "%");
-                        document.getElementById('sign-in-status').textContent = '上傳中！';
-                        state.innerHTML = "上傳中...";
-                    },
-                    function error(err) {
-                        state.innerHTML = "上傳錯誤，請重新上傳";
-                    },
-                        
-                    // upload complete
-                    function complete() {
-                    
-                    
-                    state.innerHTML = "上傳完成!";
-                    document.getElementById('sign-in-status').textContent = '上傳成功！';
-                    document.getElementById('uploadArea').style.backgroundImage = "url('3.png')";
-                    document.getElementsByTagName("input")[0].setAttribute("type","button");
-                    fileButton.addEventListener('change', function(e) {
-                    document.getElementById('uploadArea').style.backgroundImage = "url('icon_upload.png')";
-                    });
-                    
-                    //location.reload();
-                });
-            });
+           var uploader = document.getElementById('uploader');
+             var fileButton = document.getElementById('fileButton');
+             var state = document.getElementById('state');
+             // listen for file selection
+             fileButton.addEventListener('change', function (e) {
+                 //get file
+                 var file = e.target.files[0];
+                 //create ref
+                 var storageRef = firebase.storage().ref(user.displayName.toString());
+                 var imgRef = storageRef.child(file.name + '-' + Date());
+                 //upload file
+                 var task = imgRef.put(file);
+                 //update progress bar
+                 task.on('state_changed',
+                     function progress(snapshot) {
+                         var percentage = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
+                         uploader.value = percentage;
+                         $("#uploader")
+                             .css("width", percentage + "%")
+                             .attr("aria-valuenow", percentage)
+                             .text(percentage + "%");
+                         document.getElementById('sign-in-status').textContent = '太好了，圖片正在上傳中喔！';
+                         state.innerHTML = "上傳中...";
+                     },
+                     function error(err) {
+                         state.innerHTML = "上傳錯誤，請再重新上傳一次ＱＱ";
+                     },
+                     function complete() {
+                         state.innerHTML = "上傳成功:D";
+                         document.getElementById('sign-in-status').textContent = '噹噹噹～～～上傳完成!';
+//                         location.reload();
+                     });
+             });
         } else {
             // User is signed out.
             document.getElementById('sign-in-status').textContent = '請先登入喔!';
